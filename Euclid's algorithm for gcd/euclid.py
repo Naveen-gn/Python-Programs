800e8d6f591f64ba0e97d67cbfaa62a8df7096fa
Naveen-gn <naveen2003gn@gmail.com>
2024-01-25 18:25:48 +0530

    Another optimal Euclid's algorithm for gcd of two numbers

--- Euclid's algorithm for gcd/euclid.py
+++ Euclid's algorithm for gcd/euclid.py
@@ -23,4 +23,18 @@ def gcd(m,n):
 m=int(input("Enter the first number: "))   
 n=int(input("Enter the second number: "))
 result =gcd(m,n)
-print("GCD of ",m,",",n," : ",result)    
+print("GCD of ",m,",",n," : ",result)
+
+#Another optimal Euclid's algorithm for gcd of two numbers
+def gcd(m,n):
+    if m<n:
+        (m,n)=(n,m)
+    if (m%n==0):
+        return n
+    else:
+        r=m%n
+        return gcd(n,r)
+m=int(input("Enter the first number: "))   
+n=int(input("Enter the second number: "))
+result =gcd(m,n)
+print("GCD of ",m,",",n," : ",result)
