2b50a71774b157208673d73ede486a47cbf91d10
Naveen-gn <naveen2003gn@gmail.com>
2024-01-25 18:52:55 +0530

    Second question for week 1

--- NPTEL/week1.py
+++ NPTEL/week1.py
@@ -6,3 +6,15 @@ def f(x):
   return(d)
 result=f(27182818)
 print(result)
+
+#Second question
+def h(n):
+    s = 0
+    for i in range(2,n):
+        if n%i == 0:
+           s = s+i
+    return(s)
+ans1= h(60)
+ans2= h(45)
+result=ans1-ans2
+print(result)
