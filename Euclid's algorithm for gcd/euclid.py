e029a576dd60d644f2954535660b86dfd917f69c
Naveen-gn <naveen2003gn@gmail.com>
2024-01-25 18:12:35 +0530

    Another Euclid's algorithm for gcd of two numbers using while loop

--- Euclid's algorithm for gcd/euclid.py
+++ Euclid's algorithm for gcd/euclid.py
@@ -11,3 +11,16 @@ m=int(input("Enter the first number: "))
 n=int(input("Enter the second number: "))
 result =gcd(m,n)
 print("GCD of ",m,",",n," : ",result)
+
+#Another Euclid's algorithm for gcd of two numbers using while loop
+def gcd(m,n):
+    if m<n:
+       (m,n)=(n,m)
+    while (m%n!=0):
+        diff=m-n
+        (m,n)=(max(n,diff),min(n,diff))
+    return n
+m=int(input("Enter the first number: "))   
+n=int(input("Enter the second number: "))
+result =gcd(m,n)
+print("GCD of ",m,",",n," : ",result)    
