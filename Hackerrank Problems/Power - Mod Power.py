8fdc4ffb20f8a01fa1725babf247fb68f4f788e5
Naveen-gn <naveen2003gn@gmail.com>
2024-01-26 22:02:09 +0530

    power and remainder program

---
+++ Hackerrank Problems/Power - Mod Power.py
@@ -0,0 +1,14 @@
+'''
+You are given three integers: a, b, and m. Print two lines.
+On the first line, print the result of pow(a,b). On the second line, print the result of pow(a,b,m).
+
+Input Format
+The first line contains a, the second line contains b, and the third line contains m.
+'''
+a=int(input())
+b=int(input())
+m=int(input())
+power=pow(a,b)
+rem=pow(a,b,m)
+print(power)
+print(rem)
